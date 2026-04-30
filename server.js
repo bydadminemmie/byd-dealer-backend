6e65670dce3c207932e76adbc5a8cea05c5156d8
bydadminemmie <rryanhoward09@gmail.com>
2026-05-01 00:27:15 +0100

    Fix CORS for port 5501

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5000',
   'http://127.0.0.1:5500',
+  'http://127.0.0.1:5501',
+  'http://localhost:5501',
   'https://emmie-backend.onrender.com',
 ];
 
